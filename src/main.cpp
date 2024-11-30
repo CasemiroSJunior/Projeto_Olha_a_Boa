0a61ae50a5320ad73fc638aa58427b0dac7b9343
CASEMIRO SENEME JUNIOR <casemiro.seneme@fatecararas.com>
2024-11-30 11:55:58 -0300

    [chore]: Implement basic testing for sheet on main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,39 @@
 using namespace std;
 
 int main() {
+    int qntd;
     cout << "Olá, seja bem vindo ao gerenciador de cartelas." << endl;
     cout << "Para poder começar, responda. " << endl;
     cout << "Quantas cartelas deseja gerenciar?" << endl;
+    
+    cin >> qntd;
+    CARTELA vetor_cartela[qntd];
+
+    for (int i = 0; i < qntd; i++){
+        gerarCartela(&vetor_cartela[i]);
+    }
+    for (int i = 0; i < qntd; i++){
+        cout << "Cartela " << i+1 << endl;
+        visualizarCartela(&vetor_cartela[i]);
+    }
+
+    int lastInput;
+    while (lastInput != 0) {
+        cout << "Digite o número sorteado ou 0 para sair" << endl;
+        cin >> lastInput;
+        if (lastInput == 0)
+            break;
+        for (int i = 0; i < qntd; i++){
+            marcarNumero(&vetor_cartela[i], lastInput);
+            cout << "Cartela " << i+1 << endl;
+            visualizarCartela(&vetor_cartela[i]);
+        }
+    }
+    
+
+    
+    
+
 
     
 
